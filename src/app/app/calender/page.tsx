989ad83f412daac7f33dc1f58c48192e048c4f62
Chrispine Owuor <104433015+ChrisOwuor@users.noreply.github.com>
2026-06-17 01:57:40 +0300

    feat:worked on secondary sidebar adding fuctinlity to add spaces,folders and pages

--- src/app/app/calender/page.tsx
+++ src/app/app/calender/page.tsx
@@ -1,16 +1,21 @@
+import { auth } from "../../../../lib/auth";
 
+export default async function page() {
+  const session = await auth();
 
+  if (!session?.user) {
+    return <div>Unauthorized </div>;
+  }
 
-export default function page() {
   return (
     <div className="cont p-2 w-full h-full">
       <div
         id="application-container"
         className="w-full h-full transition-colors left-[72px] grid grid-cols-5 duration-200 px-5 py-6  shadow-custom rounded-sm "
       >
-        calender
+        calender jjkj
       </div>
-      ;
+      {session.user.name};
     </div>
   );
 }
